6858b8c138d2493c58d8b8b2d7f6e99da1ab5214
portysnorty <64874264+portysnorty@users.noreply.github.com>
2024-10-09 11:45:17 -0600

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -66,16 +66,18 @@ def Partition(numbers, lowIndex, highIndex):
 
     return highIndex
 
-def quicksort(numbers, lowIndex=0, highIndex=0):
-    highIndex = len(numbers)-1
+def quicksort(ls):
+    Quickestsort(ls,0, len(ls)-1)
+    return ls
+
+def Quickestsort(numbers, lowIndex, highIndex):
     if lowIndex >= highIndex:
       return 
 
     lowEndIndex = Partition(numbers, lowIndex, highIndex)
    
-    quicksort(numbers, lowIndex, lowEndIndex)
-    quicksort(numbers, lowEndIndex + 1, highIndex)
-    return numbers
+    Quickestsort(numbers, lowIndex, lowEndIndex)
+    Quickestsort(numbers, lowEndIndex + 1, highIndex)
 
 #shell(list) -> sorted list
 def thisSort(ls, start, gap):
